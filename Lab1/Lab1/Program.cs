59b788b5bed6c778d9a3b1303881fed847967efc
vpape <38445312+vpape@users.noreply.github.com>
2018-04-17 20:08:00 -0500

    Lab 3

--- Lab1/Lab1/Program.cs
+++ Lab1/Lab1/Program.cs
@@ -6,12 +6,35 @@ namespace Lab1
     {
         static void Main(string[] args)
         {
-            String strPlayerName = "Nolan Ryan";
+            
             double nHits= 2, nAtBats= 5;
             
 
             Console.WriteLine("This program calculates a baseball player's batting average");
-            Console.WriteLine("Player Name:" + " " + strPlayerName);
+            Console.WriteLine("Player Name: ");
+            
+
+            String strPlayerName = Console.ReadLine();
+
+            Console.Write("Enter Player 2: ");
+
+            String player2 = Console.ReadLine();
+
+            if ((strPlayerName == "Vernon" || player2 == "Vernon") || 
+                (strPlayerName == "Matt" || player2 == "Matt"))
+            {
+                Console.WriteLine("Nah bro, give me someone else");
+                strPlayerName = Console.ReadLine();
+            }
+            else if(strPlayerName == "Nader")
+            {
+                Console.WriteLine("Yeah he's a cool guy");
+            }
+            else
+            {
+                Console.WriteLine("Thanks for not giving me Vernon or Matt");
+            }
+
             Console.WriteLine("Enter Player Name:");
             strPlayerName = Console.ReadLine();
             
@@ -22,11 +45,15 @@ namespace Lab1
             nAtBats =  Int32.Parse(Console.ReadLine());
 
             double dblBattingAverage = nHits / nAtBats;
+
+            Console.WriteLine();
             Console.WriteLine(strPlayerName);
             Console.WriteLine(nHits + " hits, " + nAtBats + " bats" );
-            Console.WriteLine("Batting Avg:" + dblBattingAverage);
+            Console.WriteLine("Batting Avg: " + dblBattingAverage);
             Console.Read();
             
+
+
             
             
 
---
+++ Lab3/Lab3/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace Lab3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double weight;
+            double height;
+
+            Console.WriteLine("Enter your weight pounds: ");
+            weight = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter your height inches: ");
+            height = Int32.Parse(Console.ReadLine());
+
+            
+
+            double BMI = (weight * 703) / (height * height);
+            
+
+            if(BMI < 18.5)
+            {
+                Console.WriteLine("Your BMI is: " + BMI);
+                Console.WriteLine("Underweight");                
+
+            }
+
+            else if (BMI >= 18.5 && BMI <= 25)
+            {
+                Console.WriteLine("Your BMI is: " + BMI);
+                Console.WriteLine("Normal");
+              
+
+            }
+
+            else if (BMI >= 25 && BMI <= 29.9)
+            {
+                Console.WriteLine("Your BMI is: " + BMI);
+                Console.WriteLine("Overweight");       
+
+
+            }
+
+            else
+            {
+                Console.WriteLine("Your BMI is: " + BMI);
+                Console.WriteLine("Obese");
+                
+
+
+            }
+
+            Console.Read();
+
+
+        }
+    }
+}
