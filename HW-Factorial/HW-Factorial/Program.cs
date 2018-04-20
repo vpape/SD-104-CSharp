6a95267ac78598e51f010a9883a32e14f7dfd489
vpape <38445312+vpape@users.noreply.github.com>
2018-04-20 17:56:34 -0500

    HW- Factorial

---
+++ HW-Factorial/HW-Factorial/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace HW_Factorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int userinput;
+            int total = 1;
+
+
+            Console.WriteLine("Enter number to factor: ");
+            while (Int32.TryParse(Console.ReadLine(), out userinput) == false)
+
+            {
+                Console.WriteLine("Give me actual number: ");
+                          
+            }
+
+            for (int i = userinput; i > 0; i--)
+            {
+                total *= i;
+
+                if( i > 1)
+                Console.Write(i+ "x");
+                else
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            
+            Console.Read();
+
+        }
+    }
+}
+
+
