b964bec2a426d6a71aa166358c7cc2d18c0055ab
vpape <38445312+vpape@users.noreply.github.com>
2018-04-23 17:44:54 -0500

    changed for loop

--- Find the Max and Min/Find the Max and Min/Program.cs
+++ Find the Max and Min/Find the Max and Min/Program.cs
@@ -13,10 +13,9 @@ namespace Find_the_Max_and_Min
             //Program will output this to the user
 
             //hint #1
-            int min;
-            int max;
-
-            //hint #2
+            //use if statement for min[position] and max[position]
+            //min = array[position]
+            //hint #2--do not do in loop
             //min and max is going to start as the first value in your array. so array[0]
 
             //hint #3
@@ -24,7 +23,49 @@ namespace Find_the_Max_and_Min
             //max means array[position] > max
 
 
+            Console.WriteLine("Please enter how many values you want in your array: ");
+            int input;
 
+            while (Int32.TryParse(Console.ReadLine(), out input) == false)
+            {
+                Console.Write("Not a number, please enter the values");
+            }
+
+            int[] number = new int[input];
+            for (int i = 0; i < number.Length; i++)
+            {
+                Console.Write("Please enter the value for number " + (i+ 1) + ": ");
+                while (Int32.TryParse(Console.ReadLine(), out input) == false)
+                {
+                   Console.Write("Not a number, Please enter a number");
+                   
+                }
+                number[i] = input;
+            }
+
+            int min = number[0];
+            int max = number[0];
+
+            for (int i = 0; i < number.Length; i++)
+            {
+
+                if (max < number[i])
+                {
+                    max = number[i];
+                }
+
+                if (min > number[i])
+                {
+                    min = number[i];
+                }
+                               
+            }
+            
+            Console.WriteLine("The maximum is: " + max);
+            Console.WriteLine("The minimum is: " + min);
+            Console.Read();
+
+            
         }
     }
 }
