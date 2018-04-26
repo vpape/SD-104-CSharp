9df316724504cc1f8b9fad82a629867bfafc6a14
vpape <38445312+vpape@users.noreply.github.com>
2018-04-25 21:38:29 -0500

    added conversion class

---
+++ Lab 4/Lab 4/Conversion.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HW1
+{
+     class Conversion
+     {
+        double number;
+
+        public Conversion()
+        {
+            number = 0;
+        }
+
+        public void getUserInput()
+        {
+            number = Int32.Parse(Console.ReadLine());
+            
+        }
+
+        public void ConvertMilesToKm()
+        {
+            getUserInput();
+            Console.WriteLine(number+ " Miles converts to " + (1.6d * number) + " Kilometers.");
+            
+        }
+
+        public void ConvertInchesToCm()
+        {
+            getUserInput();
+            Console.WriteLine(number + " Inches converts to " + (2.54d * number) + " Centimeters.");
+            
+        }
+
+        public void ConvertLbsToKg()
+        {
+            getUserInput();
+            Console.WriteLine(number + " Pounds converts to " + (0.45359237d * number) + " Kilograms.");
+            
+        }
+
+        public void ConvertKmToMiles()
+        {
+            getUserInput();
+            Console.WriteLine(number + " Kilometers converts to " + (0.62137119d * number) + " Miles.");
+            
+        }
+
+        public void ConvertCmToInches()
+        {
+            getUserInput();
+            Console.WriteLine(number + " Centimeters converts to " + (0.39d * number) + " Inches.");
+            
+        }
+
+        public void ConvertKgToLbs()
+        {
+            getUserInput();
+            Console.WriteLine(number + " Kilos converts to " + (2.2d * number) + " Lbs.");
+            
+        }
+
+
+     }
+    
+        
+}
+
--- Lab 4/Lab 4/Program.cs
+++ Lab 4/Lab 4/Program.cs
@@ -1,12 +1,15 @@
 ﻿using System;
 
-namespace Lab_4
+namespace HW1
 {
     class Program
     {
         static void Main(string[] args)
         {
+
+
             String choice;
+            Conversion conversion = new Conversion();
 
 
             //Miles to KM: 1.6 * miles -a
@@ -19,61 +22,80 @@ namespace Lab_4
             //kg to lbs: 2.2 * kg - f
 
 
-
+            
 
             Console.WriteLine("This program has multiple functionalities" );
             Console.WriteLine("A to convert miles to KM, B to convert inches to CM, C to convert lbs to kg");
             Console.WriteLine("D to convert KM to miles, E to convert CM to inches, F to convert kg to lbs");
-            Console.WriteLine("Enter choice: ");
+            Console.Write("Enter choice: ");
             choice = Console.ReadLine();
 
 
             switch (choice.ToUpper())
             {
                 case "A":
-                    Console.WriteLine("Enter the Miles to convert: ");
-                    double miles = Int32.Parse(Console.ReadLine());
+                    Console.Write("Enter how many miles to convert: ");                    
+                    conversion.ConvertMilesToKm(); //converts miles to Km
+
+                    /*double miles = Int32.Parse(Console.ReadLine());
                     double km = miles * 1.6;
-                    Console.WriteLine(miles + " miles is = " + km + " kilometers");
+                    Console.WriteLine(miles + " miles is = " + km + " kilometers");*/
+
                     break;
 
                    
                 case "B":
-                    Console.WriteLine("Enter the inches to convert: ");
-                    double inches = Int32.Parse(Console.ReadLine());
+                    Console.Write("Enter how many inches to convert: ");                    
+                    conversion.ConvertInchesToCm(); //converts miles to Km
+
+                    /*double inches = Int32.Parse(Console.ReadLine());
                     double cm = 2.54 * inches;
-                    Console.WriteLine(inches + " inches is = " + cm + " centimeters");
+                    Console.WriteLine(inches + " inches is = " + cm + " centimeters");*/
+
                     break;
 
                 case "C":
-                    Console.WriteLine("Enter the lbs to convert: ");
-                    double lbs = Int32.Parse(Console.ReadLine());
+                    Console.Write("Enter how many lbs to convert: ");                   
+                    conversion.ConvertLbsToKg(); //converts miles to Km
+
+                    /*double lbs = Int32.Parse(Console.ReadLine());
                     double kg = 0.45359237 * lbs;
-                    Console.WriteLine(lbs + " pounds is = " + kg + " kilograms");
+                    Console.WriteLine(lbs + " pounds is = " + kg + " kilograms");*/
+
                     break;
 
                 case "D":
-                    Console.WriteLine("Enter the km to convert: ");
-                    double Km = Int32.Parse(Console.ReadLine());
+                    Console.Write("Enter how many km to convert: ");                    
+                    conversion.ConvertKmToMiles(); //converts miles to Km
+
+                    /*double Km = Int32.Parse(Console.ReadLine());
                     double Miles = 0.62137119 * Km;
-                    Console.WriteLine(Km + " kilometers is = " + Miles + " miles");
+                    Console.WriteLine(Km + " kilometers is = " + Miles + " miles");*/
+
                     break;
 
                 case "E":
-                    Console.WriteLine("Enter the cm to convert: ");
-                    double Cm = Int32.Parse(Console.ReadLine());
+                    Console.Write("Enter how many cm to convert: ");                    
+                    conversion.ConvertCmToInches(); //converts miles to Km
+
+                    /*double Cm = Int32.Parse(Console.ReadLine());
                     double Inches = 0.39 * Cm;
-                    Console.WriteLine(Cm + " kilograms is = " + Inches + " pounds");  
+                    Console.WriteLine(Cm + " kilograms is = " + Inches + " pounds");*/
+
                     break;
 
                 case "F":
-                    Console.WriteLine("Enter the kg to convert: ");
-                    double Kg = Int32.Parse(Console.ReadLine());
+                    Console.Write("Enter how many kg to convert: ");                    
+                    conversion.ConvertKgToLbs(); //converts miles to Km
+
+                    /*double Kg = Int32.Parse(Console.ReadLine());
                     double Lbs = 2.2 * Kg;
-                    Console.WriteLine(Kg + " kilograms is = " + Lbs + " pounds");
+                    Console.WriteLine(Kg + " kilograms is = " + Lbs + " pounds");*/
+
                     break;
 
                 default:
+
                     break;
 
                     
